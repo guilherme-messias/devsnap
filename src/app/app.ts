52cff04fba324bea56924e0e2ed0fdd037cdd9f8
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-19 17:02:25 -0300

    add: inicia aplicacao em angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Aula } from "./aula/aula";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Aula],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
