afd68ccebe5f5ec64a4f1867b4ac0c78f02677a2
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-21 12:46:13 -0300

    refactor: update nota service methods and improve nota-component template

--- src/app/services/nota.service.ts
+++ src/app/services/nota.service.ts
@@ -4,12 +4,15 @@ import { Injectable } from '@angular/core';
 export class NotaService {
   notas: number[] = [8, 6, 9];
 
-  adicionar(nota: number) {
-    this.notas.push(nota);
+  adicionarNota(nota: number) {
+    this.notas = [...this.notas, nota];
   }
 
-  media(): number {
-    const soma = this.notas.reduce((acc, n) => acc + n, 0);
-    return soma / this.notas.length;
+  removerNota(nota: number) {
+    this.notas = this.notas.filter((_, i) => i !== nota);
+  }
+
+  calcularTotal(): number {
+    return this.notas.reduce((total, nota) => total + nota, 0);
   }
 }
