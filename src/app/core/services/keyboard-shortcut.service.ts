bed97908a34ad8584bcede42d4f2d46bb78320f9
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-24 15:02:53 -0300

    feat: enhance KeyboardShortcutService to support platform detection for keyboard shortcuts

--- src/app/core/services/keyboard-shortcut.service.ts
+++ src/app/core/services/keyboard-shortcut.service.ts
@@ -1,11 +1,15 @@
-import { inject, Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 @Injectable({ providedIn: 'root' })
 export class KeyboardShortcutService {
   private readonly _dialog: MatDialog = inject(MatDialog);
+  private readonly _platformId = inject(PLATFORM_ID);
 
   init(): void {
+    if (!isPlatformBrowser(this._platformId)) return;
+
     document.addEventListener('keydown', (event) => {
       if (event.ctrlKey && event.shiftKey && event.key === 'N') {
         event.preventDefault();
