c822c675bf28fa5ae0e7eca83540d4e9d68a8ab3
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-24 14:12:30 -0300

    feat: update FabCaptureComponent to use Material Button and Dialog, add hidden input property

--- src/app/shared/components/fab-capture.component.ts
+++ src/app/shared/components/fab-capture.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { MatIcon } from "@angular/material/icon";
+import { Component, inject, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-fab-capture',
   standalone: true,
+  imports: [MatIconModule, MatButtonModule],
   template: `
-    <button mat-fab color="primary" aria-label="Capturar tela">
-      <mat-icon>camera</mat-icon>
+    <button mat-flat-button color="primary" aria-label="Capturar tela" [class.hidden]="hidden">
+      <mat-icon>add</mat-icon>
     </button>
   `,
-  imports: [MatIcon],
 })
-export class FabCaptureComponent {}
+export class FabCaptureComponent {
+  @Input() hidden = false;
+
+  private _dialog = inject(MatDialog);
+
+  openDialog(): void {
+    // TODO: componente será criado
+    // this._dialog.open(QuickCaptureDialogComponent);
+  }
+}
