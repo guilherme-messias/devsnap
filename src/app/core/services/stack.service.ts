c359c422056c169c089cccf695b4be7bf7376a8d
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-24 08:49:11 -0300

    feat: implement stack management methods in StackService

--- src/app/core/services/stack.service.ts
+++ src/app/core/services/stack.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Stack } from '../models/stack.model';
 
 @Injectable({ providedIn: 'root' })
 export class StackService {
   private _stacks = signal<Stack[]>(this._load());
+  readonly stacks = this._stacks.asReadonly();
 
   private _load(): Stack[] {
     return localStorage.getItem('devsnap:stacks')
@@ -13,5 +14,26 @@ export class StackService {
 
   private _persist() {
     localStorage.setItem('devsnap:stacks', JSON.stringify(this._stacks()));
+  }
+
+  add(stack: Stack): void {
+    this._stacks.update((stacks) => [...stacks, stack]);
+    this._persist();
+  }
+
+  remove(stack: Stack): void {
+    this._stacks.update((stacks) => stacks.filter((s) => s.id !== stack.id));
+    this._persist();
+  }
+
+  update(stack: Stack, changes: Partial<Stack>): void {
+    this._stacks.update((stacks) =>
+      stacks.map((s) => (s.id === stack.id ? { ...s, ...changes } : s)),
+    );
+    this._persist();
+  }
+
+  getById(id: string): Stack | undefined {
+    return computed(() => this._stacks().find((s) => s.id === id))?.();
   }
 }
