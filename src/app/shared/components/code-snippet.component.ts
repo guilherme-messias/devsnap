7a1fce46f3c7a1cdae4309fe7ba4b496ae2e54d0
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-24 12:25:08 -0300

    feat: add app-code-snippet

---
+++ src/app/shared/components/code-snippet.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { MarkdownComponent } from 'ngx-markdown';
+
+@Component({
+  selector: 'app-code-snippet',
+  imports: [MarkdownComponent],
+  standalone: true,
+  template: `
+    <div class="bg-gray-800 text-white p-4 rounded-lg">
+      <markdown [data]="code" ngPreserveWhitespaces> </markdown>
+    </div>
+  `,
+})
+export class CodeSnippetComponent {
+  @Input() code: string = '';
+}
