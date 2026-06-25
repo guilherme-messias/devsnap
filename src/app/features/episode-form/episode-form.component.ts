aa61a14be37591caa05a245b635a975d49226d53
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-25 18:23:57 -0300

    feat: implement reactive form in EpisodeFormComponent with validation for episode data fields

--- src/app/features/episode-form/episode-form.component.ts
+++ src/app/features/episode-form/episode-form.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-episode-form',
   standalone: true,
-  template: `<p>Episode Form</p>`,
+  templateUrl: './episode-form.component.html',
+  imports: [MatFormFieldModule, MatSelectModule, ReactiveFormsModule],
 })
-export class EpisodeFormComponent {}
+export class EpisodeFormComponent {
+  form: FormGroup;
+
+  private readonly _fb = inject(FormBuilder);
+
+  constructor(_fb: FormBuilder) {
+    this.form = this._fb.group({
+      episodeData: this._fb.group({
+        stackId: ['', Validators.required],
+        title: ['', Validators.required],
+        error: ['', Validators.required],
+        attempts: ['', Validators.required],
+        solution: ['', Validators.required],
+        reasoning: ['', Validators.required],
+      }),
+
+      snippet: [''],
+    });
+  }
+}
