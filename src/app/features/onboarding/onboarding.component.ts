e087a1870977d435b636c15b0b84f3c1434652ec
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-25 16:45:02 -0300

    feat: implement onboarding component with form handling for stack creation and navigation

--- src/app/features/onboarding/onboarding.component.ts
+++ src/app/features/onboarding/onboarding.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { StackService } from '../../core/services/stack.service';
+import { v4 as uuidv4 } from 'uuid';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-onboarding',
-  standalone: true,
-  template: `<p>Onboarding</p>`,
+  imports: [MatFormFieldModule, MatInputModule],
+  templateUrl: './onboarding.component.html',
 })
-export class OnboardingComponent {}
+export class OnboardingComponent {
+  private readonly _stackService: StackService = inject(StackService);
+  private readonly _router: Router = inject(Router);
+  readonly form = new FormGroup({
+    name: new FormControl<string>(''),
+  });
+
+  createStack(): void {
+    this._stackService.add({
+      id: uuidv4(),
+      name: this.form.get('name')?.value || '',
+      createdAt: new Date(),
+    });
+    this._router.navigate(['/stacks', this._stackService.stacks().at(-1)?.id]);
+  }
+}
