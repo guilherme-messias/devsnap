91ae93f1ed4b188de1b109420e4dd79d188f2227
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-26 12:55:41 -0300

    feat: add tag management functionality to EpisodeFormComponent with methods for adding and removing tags

--- src/app/features/episode-form/episode-form.component.ts
+++ src/app/features/episode-form/episode-form.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { StackService } from '../../core/services/stack.service';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
   selector: 'app-episode-form',
@@ -13,8 +15,13 @@ import { StackService } from '../../core/services/stack.service';
 export class EpisodeFormComponent {
   form: FormGroup;
 
+  tags = signal<string[]>([]);
+  currentTag = signal<string>('');
+
   private readonly _stackService = inject(StackService);
   readonly stacks = this._stackService.stacks;
+
+  readonly separatorKeysCodes = [ENTER, COMMA];
 
   private readonly _fb = inject(FormBuilder);
 
@@ -31,5 +38,17 @@ export class EpisodeFormComponent {
 
       snippet: [''],
     });
+  }
+
+  addTag(event: MatChipInputEvent): void {
+    const value = event.value.trim();
+    if (value) {
+      this.tags.update((tags) => [...tags, value]);
+    }
+    this.currentTag.set('');
+  }
+
+  removeTag(tag: string): void {
+    this.tags.update((tags) => tags.filter((t) => t !== tag));
   }
 }
