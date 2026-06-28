87942d23c66719cb1909b04289998f3d86596d77
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-28 15:25:24 -0300

    feat: integrate FocusSessionService and implement reactive signal for current episode in focus session component

--- src/app/features/focus/focus-session.component.ts
+++ src/app/features/focus/focus-session.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FocusSessionService } from '../../core/services/focus-session.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-focus-session',
   standalone: true,
   templateUrl: './focus-session.component.html',
 })
-export class FocusSessionComponent {}
+export class FocusSessionComponent {
+  private readonly _focusSessionService = inject(FocusSessionService);
+
+  readonly currentEpisode = toSignal(this._focusSessionService.currentEpisode$, {
+    initialValue: null,
+  });
+}
