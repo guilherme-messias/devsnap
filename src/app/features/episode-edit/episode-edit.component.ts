d230bb4065fe688d7ad060a33eea487fd5851a51
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-28 12:12:57 -0300

    feat: implement episode editing functionality with route-based episode retrieval and signal management

--- src/app/features/episode-edit/episode-edit.component.ts
+++ src/app/features/episode-edit/episode-edit.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { EpisodeService } from '../../core/services/episode.service';
+import { StackService } from '../../core/services/stack.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Episode } from '../../core/models/episode.model';
 
 @Component({
   selector: 'app-episode-edit',
   standalone: true,
-  template: `<p>Episode Edit</p>`,
+  templateUrl: './episode-edit.component.html',
 })
-export class EpisodeEditComponent {}
+export class EpisodeEditComponent {
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _episodeService = inject(EpisodeService);
+
+  readonly episode = signal<Episode | undefined>(undefined);
+
+  ngOnInit(): void {
+    const episodeId = this._route.snapshot.paramMap.get('id');
+    if (episodeId) {
+      this.episode.set(this._episodeService.getById(episodeId));
+    }
+  }
+}
