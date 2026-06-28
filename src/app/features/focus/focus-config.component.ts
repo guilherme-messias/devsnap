2446e3b51ad4e8815b2e126bf3c03af1e211738e
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-28 12:53:01 -0300

    feat: add focus configuration component with stack selection functionality

--- src/app/features/focus/focus-config.component.ts
+++ src/app/features/focus/focus-config.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { EpisodeService } from '../../core/services/episode.service';
+import { StackService } from '../../core/services/stack.service';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-focus-config',
   standalone: true,
-  template: `<p>Focus Config</p>`,
+  templateUrl: 'focus-config.component.html',
+  imports: [MatSelectModule],
 })
-export class FocusConfigComponent {}
+export class FocusConfigComponent {
+  private readonly _stackService: StackService = inject(StackService);
+  private readonly _episodeService: EpisodeService = inject(EpisodeService);
+
+  readonly stacks = this._stackService.stacks;
+}
