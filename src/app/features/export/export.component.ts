7740c627d38c0116dd4d1592272bd7273321ede3
Guilherme Messias <messiasguilherme700@gmail.com>
2026-06-29 10:59:31 -0300

    feat: refactor ExportComponent to use external template and inject services for improved functionality

--- src/app/features/export/export.component.ts
+++ src/app/features/export/export.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { StackService } from '../../core/services/stack.service';
+import { EpisodeService } from '../../core/services/episode.service';
+import { ExportService } from '../../core/services/export.service';
 
 @Component({
   selector: 'app-export',
   standalone: true,
-  template: `<p>Export</p>`,
+  templateUrl: './export.component.html',
 })
-export class ExportComponent {}
+export class ExportComponent {
+  private readonly _stackService = inject(StackService);
+  private readonly _episodeService = inject(EpisodeService);
+  private readonly _exportService = inject(ExportService);
+}
