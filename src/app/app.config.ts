541100f6121c80a4f8b46d4dbc6b2fc8400808f3
Guilherme Messias <messiasguilherme700@gmail.com>
2026-09-14 14:04:25 -0300

    chore: remove server-side rendering configurations and related files from the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
@@ -9,6 +8,5 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
     provideMarkdown(),
-    provideClientHydration(),
   ],
 };
